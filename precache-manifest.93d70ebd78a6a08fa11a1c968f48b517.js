d53e168ed25d0bb496f4f0650a8d5097b9fc6812
Ola-22 <olanazly@gmail.com>
2020-11-05 09:31:21 +0200

    Updates

---
+++ precache-manifest.93d70ebd78a6a08fa11a1c968f48b517.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2cb39626871b09c1474939fa9ecb7a",
+    "url": "/FormValidation/index.html"
+  },
+  {
+    "revision": "8b0bede5b5fe4538da46",
+    "url": "/FormValidation/static/css/main.f6617d12.chunk.css"
+  },
+  {
+    "revision": "4275295d9d73f7ba4903",
+    "url": "/FormValidation/static/js/2.cca75c89.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/FormValidation/static/js/2.cca75c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0bede5b5fe4538da46",
+    "url": "/FormValidation/static/js/main.84339624.chunk.js"
+  },
+  {
+    "revision": "97ee35253ffa4738f0d9",
+    "url": "/FormValidation/static/js/runtime-main.143ea4d7.js"
+  },
+  {
+    "revision": "5ff1f2a9a78730d7d0c309320ff3c9c7",
+    "url": "/FormValidation/static/media/Inter-Medium-slnt=0.5ff1f2a9.ttf"
+  },
+  {
+    "revision": "a68e1eb70669da693d4bdc3ece1d1762",
+    "url": "/FormValidation/static/media/img.a68e1eb7.jpg"
+  },
+  {
+    "revision": "40eae87fc3741c25a06d1bdf12a5d2be",
+    "url": "/FormValidation/static/media/superscene.40eae87f.png"
+  }
+]);
